5ecdb798998803a3c900f8c535e8ab572769e132
Angelos <Angelos@DESKTOP-AFUKVS8.fritz.box>
2018-10-05 15:56:30 +0200

    added new Constructor for new Ai (DMTCS)

--- Xeri/src/MonteCarloTreeSearch/SimulatedBoard.java
+++ Xeri/src/MonteCarloTreeSearch/SimulatedBoard.java
@@ -1,6 +1,9 @@
 package MonteCarloTreeSearch;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+
+import org.apache.commons.lang3.SerializationUtils;
 
 import TheGame.*;
 
@@ -8,15 +11,16 @@ import TheGame.*;
 public class SimulatedBoard implements Serializable{
 
 	CardPile p1Hand;
+	CardPile p2Hand;
 	int p2Handsize;
 
 	CardPile p1Collect;
 	CardPile p2Collect;
 
 	CardPile field;
-	CardPile deck;
 	
-
+	
+	
 	public static final int IN_PROGRESS = -1;
 	public static final int DRAW = 0;
 	public static final int P1 = 1;
@@ -25,39 +29,132 @@ public class SimulatedBoard implements Serializable{
 	public SimulatedBoard() {
 		
 	}
-
-	public SimulatedBoard(String s) {
-		p1Hand = new CardPile();
-		CardPile p2Hand = new CardPile();
-		p2Handsize = Board.getInstance().getOppHand().getComponentCount();
-		
-		for (int i = 0; i < Board.getInstance().getPlayerHand().getComponentCount(); i++) {
-			p1Hand.add((Card) Board.getInstance().getPlayerHand().getComponent(i));
+/**
+ * Constructor for the Deterministic Board; the Opp is given a random hand of 6 Cards	
+ * @param player
+ */
+	public SimulatedBoard(boolean player) {
+		if(player){
+			p1Hand = new CardPile();
+			p2Hand = new CardPile();
+			CardPile realP2Hand = new CardPile();
+			p2Handsize = Board.getInstance().getOppHand().getComponentCount();
+			ArrayList<Integer> randomCards = new ArrayList<Integer>();
+			CardPile tmpDeck = SerializationUtils.clone(Board.getInstance().getDeck());
+			
+			for (int i = 0; i < Board.getInstance().getPlayerHand().getComponentCount(); i++) {
+				p1Hand.add((Card) Board.getInstance().getPlayerHand().getComponent(i));
+			}
+			
+			for (int i = 0; i < Board.getInstance().getOppHand().getComponentCount(); i++) {
+				realP2Hand.add((Card) Board.getInstance().getOppHand().getComponent(i));
+			}
+			tmpDeck.addAll(realP2Hand);
+			
+			for (int i = 0; i < p2Handsize; i++) {
+				int randomCard = (int) (Math.random() * (tmpDeck.size()));
+				while(randomCards.contains(randomCard)) {
+					randomCard = (int) (Math.random() * (tmpDeck.size()));
+				}
+			randomCards.add(randomCard);
+			p2Hand.add(tmpDeck.get(randomCard));
+			}
+			
+			p1Collect = new CardPile();
+			p2Collect = new CardPile();
+			field = new CardPile(Board.getInstance().getBoardPile());
+		}else {
+			p1Hand = new CardPile();
+			p2Hand = new CardPile();
+			CardPile realP2Hand = new CardPile();
+			p2Handsize = Board.getInstance().getOppHand().getComponentCount();
+			ArrayList<Integer> randomCards = new ArrayList<Integer>();
+			CardPile tmpDeck = SerializationUtils.clone(Board.getInstance().getDeck());
+			
+			for (int i = 0; i < Board.getInstance().getOppHand().getComponentCount(); i++) {
+				p1Hand.add((Card) Board.getInstance().getOppHand().getComponent(i));
+			}
+			
+			for (int i = 0; i < Board.getInstance().getPlayerHand().getComponentCount(); i++) {
+				realP2Hand.add((Card) Board.getInstance().getPlayerHand().getComponent(i));
+			}
+			tmpDeck.addAll(realP2Hand);
+			
+			for (int i = 0; i < p2Handsize; i++) {
+				int randomCard = (int) (Math.random() * (tmpDeck.size()));
+				while(randomCards.contains(randomCard)) {
+					randomCard = (int) (Math.random() * (tmpDeck.size()));
+				}
+			randomCards.add(randomCard);
+			p2Hand.add(tmpDeck.get(randomCard));
+			}
+			
+			p1Collect = new CardPile();
+			p2Collect = new CardPile();
+			field = new CardPile(Board.getInstance().getBoardPile());
 		}
 		
-		for (int i = 0; i < Board.getInstance().getOppHand().getComponentCount(); i++) {
-			p2Hand.add((Card) Board.getInstance().getOppHand().getComponent(i));
-		}
+	}
+	
+	
+/**This Constructor is for Creating a Non-Deterministic Board
+ * where the Opponent could play everycard in the Deck at any time
+ * 
+ * @param s
+ * @param player
+ */
+	public SimulatedBoard(String s, boolean player) {
+		if(player){
+			p1Hand = new CardPile();
+			CardPile realP2Hand = new CardPile();
+			p2Handsize = Board.getInstance().getOppHand().getComponentCount();
+			
+			for (int i = 0; i < Board.getInstance().getPlayerHand().getComponentCount(); i++) {
+				p1Hand.add((Card) Board.getInstance().getPlayerHand().getComponent(i));
+			}
+			
+			for (int i = 0; i < Board.getInstance().getOppHand().getComponentCount(); i++) {
+				realP2Hand.add((Card) Board.getInstance().getOppHand().getComponent(i));
+			}
 
+			
+			p1Collect = new CardPile();
+			p2Collect = new CardPile();
+			field = new CardPile(Board.getInstance().getBoardPile());
+			p2Hand = new CardPile(Board.getInstance().getDeck());
+			p2Hand.addAll(realP2Hand);
+		}else {
+			p1Hand = new CardPile();
+			CardPile realP2Hand = new CardPile();
+			p2Handsize = Board.getInstance().getPlayerHand().getComponentCount();
+			
+			for (int i = 0; i < Board.getInstance().getOppHand().getComponentCount(); i++) {
+				p1Hand.add((Card) Board.getInstance().getOppHand().getComponent(i));
+			}
+			
+			for (int i = 0; i < Board.getInstance().getPlayerHand().getComponentCount(); i++) {
+				realP2Hand.add((Card) Board.getInstance().getPlayerHand().getComponent(i));
+			}
+
+			
+			p1Collect = new CardPile();
+			p2Collect = new CardPile();
+			field = new CardPile(Board.getInstance().getBoardPile());
+			p2Hand = new CardPile(Board.getInstance().getDeck());
+			p2Hand.addAll(realP2Hand);
+		}
 		
-		p1Collect = new CardPile(Board.getInstance().getPlayPile());
-		p2Collect = new CardPile(Board.getInstance().getOppPile());
-		field = new CardPile(Board.getInstance().getBoardPile());
-		deck = new CardPile(Board.getInstance().getDeck());
-		System.out.println(Board.getInstance().getDeck());
-		System.out.println(this.deck);
-		deck.addAll(p2Hand);
 	}
 	
 	public SimulatedBoard(SimulatedBoard sBoard) { 
 
-		this(sBoard.getP1Collect(), sBoard.getP2Collect(), sBoard.getP1Hand(), sBoard.getP2Handsize(), sBoard.getField(), sBoard.getDeck());
+		this(sBoard.getP1Collect(), sBoard.getP2Collect(), sBoard.getP1Hand(), sBoard.getP2Handsize(), sBoard.getField(), sBoard.getP2Hand());
 	}
 	
 	public SimulatedBoard(CardPile p1c, CardPile p2c, CardPile p1h, int p2size, CardPile field, CardPile deck) {
 		this.p1Collect = p1c;
 		this.p2Collect = p2c;
-		this.deck = deck;
+		this.p2Hand = deck;
 		this.field = field;
 		this.p1Hand = p1h;
 		this.p2Handsize = p2size;
@@ -74,7 +171,7 @@ public class SimulatedBoard implements Serializable{
 		if (player == P1) {
 			p1Hand.remove(c);
 		} else if (player == P2) {
-			deck.remove(c);
+			p2Hand.remove(c);
 			p2Handsize--;
 		}
 		
@@ -125,9 +222,9 @@ public class SimulatedBoard implements Serializable{
 		if(p1Hand.size() > 0 || p2Handsize > 0) {
 			return -1;
 		}else if(p1Hand.size() == 0 && p2Handsize == 0) {
-			if(p1Collect.countPoints() > p2Collect.countPoints()) {
+			if(p1Collect.countPointsForRound() > p2Collect.countPointsForRound()) {
 				return 1;
-			}else if(p1Collect.countPoints() < p2Collect.countPoints()) {
+			}else if(p1Collect.countPointsForRound() < p2Collect.countPointsForRound()) {
 				return 2;
 			}
 		}
@@ -175,12 +272,12 @@ public class SimulatedBoard implements Serializable{
 		this.field = field;
 	}
 
-	public CardPile getDeck() {
-		return deck;
+	public CardPile getP2Hand() {
+		return p2Hand;
 	}
 
-	public void setDeck(CardPile deck) {
-		this.deck = deck;
+	public void setP2Hand(CardPile deck) {
+		this.p2Hand = deck;
 	}
 	
 	public String toString() {
