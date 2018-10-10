fde1b2334a554266e8138f863a8d27894a6be909
Angelos <Angelos@DESKTOP-AFUKVS8.fritz.box>
2018-10-10 04:03:36 +0200

    added cleverPlay() method for heavy Playouts. If you can make a hit do it. light heuristic

--- Xeri/src/MonteCarloTreeSearch/State.java
+++ Xeri/src/MonteCarloTreeSearch/State.java
@@ -1,12 +1,12 @@
 package MonteCarloTreeSearch;
 
-
 import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.lang3.SerializationUtils;
 
 import TheGame.Card;
+import TheGame.CardPile;
 
 public class State {
 
@@ -36,6 +36,7 @@ public class State {
 		this.playerNo = player;
 		this.simulatedBoard = new SimulatedBoard(board);
 	}
+
 	SimulatedBoard getSimulatedBoard() {
 		return simulatedBoard;
 	}
@@ -82,38 +83,83 @@ public class State {
 		}
 	}
 
-
-
 	void togglePlayer() {
 		this.playerNo = 3 - this.playerNo;
 	}
 
 	public void randomPlay() {
-		if(playerNo == 1) {
+		if (playerNo == 1) {
 			int cardsInHand = this.simulatedBoard.getP1Hand().size();
 			Card randomCard = this.simulatedBoard.getP1Hand().get((int) (Math.random() * cardsInHand));
 			this.simulatedBoard.performMove(this.playerNo, randomCard);
-			
-		}else if(playerNo == 2) {
+
+		} else if (playerNo == 2) {
 			int possibleCard = this.simulatedBoard.getP2Hand().size();
 			Card randomCard = simulatedBoard.getP2Hand().get((int) (Math.random() * possibleCard));
 			this.simulatedBoard.performMove(this.playerNo, randomCard);
 		}
-		
-		
+	}
+
+	public void cleverPlay() {
+
+		Card cardOnBoard = null;
+		if (this.simulatedBoard.getField().size() != 0) {
+			cardOnBoard = this.simulatedBoard.getField().get(this.simulatedBoard.getField().size() - 1);
+		}
+		CardPile cardsThatTakeTrick = new CardPile();
+		Card randomCard;
+
+		if (playerNo == 1) {
+			if (cardOnBoard != null) {
+				CardPile p1Hand = this.simulatedBoard.getP1Hand();
+				for (Card card : p1Hand) {
+					if (cardOnBoard.getValue() == card.getValue()) {
+						cardsThatTakeTrick.add(card);
+					}
+				}
+				if (cardsThatTakeTrick.size() > 0) {
+					randomCard = cardsThatTakeTrick.get((int) (Math.random() * cardsThatTakeTrick.size()));
+					this.simulatedBoard.performMove(this.playerNo, randomCard);
+					return;
+				}
+			}
+
+			int cardsInHand = this.simulatedBoard.getP1Hand().size();
+			randomCard = this.simulatedBoard.getP1Hand().get((int) (Math.random() * cardsInHand));
+			this.simulatedBoard.performMove(this.playerNo, randomCard);
+
+		} else if (playerNo == 2) {
+			if (cardOnBoard != null) {
+				CardPile p1Hand = this.simulatedBoard.getP2Hand();
+				for (Card card : p1Hand) {
+					if (cardOnBoard.getValue() == card.getValue()) {
+						cardsThatTakeTrick.add(card);
+					}
+				}
+				if (cardsThatTakeTrick.size() > 0) {
+					randomCard = cardsThatTakeTrick.get((int) (Math.random() * cardsThatTakeTrick.size()));
+					this.simulatedBoard.performMove(this.playerNo, randomCard);
+					return;
+				}
+			}
+
+			int possibleCard = this.simulatedBoard.getP2Hand().size();
+			randomCard = simulatedBoard.getP2Hand().get((int) (Math.random() * possibleCard));
+			this.simulatedBoard.performMove(this.playerNo, randomCard);
+		}
 	}
 
 	public List<State> getAllPossibleStates() {
 		List<State> possibleStates = new ArrayList<>();
-		if(playerNo == 1) {
+		if (playerNo == 1) {
 			for (int i = 0; i < simulatedBoard.getP2Hand().size(); i++) {
 				SimulatedBoard newSim = (SimulatedBoard) SerializationUtils.clone(simulatedBoard);
 				State newState = new State(newSim);
-				newState.getSimulatedBoard().performMove(2, newState.getSimulatedBoard().getP2Hand().get(i));						//Syso
+				newState.getSimulatedBoard().performMove(2, newState.getSimulatedBoard().getP2Hand().get(i)); // Syso
 				possibleStates.add(newState);
 			}
 		}
-		if(playerNo == 2) {
+		if (playerNo == 2) {
 			for (int i = 0; i < simulatedBoard.getP1Hand().size(); i++) {
 				SimulatedBoard newSim = (SimulatedBoard) SerializationUtils.clone(simulatedBoard);
 				State newState = new State(newSim);
@@ -121,15 +167,14 @@ public class State {
 				possibleStates.add(newState);
 			}
 		}
-		
+
 		return possibleStates;
 	}
-	
+
 	public void add(State s) {
 		this.winCount += s.getWinScore();
 		this.visitedCount += s.getVisitCount();
 	}
-	
 
 	public State copyState(State s) {
 		State newState = new State();
@@ -137,14 +182,14 @@ public class State {
 		newState.setSimulatedBoard(s.getSimulatedBoard());
 		newState.setVisitCount(s.getVisitCount());
 		newState.setWinScore(s.getWinScore());
-		
+
 		return newState;
 	}
-	
+
 	public String toString() {
 		String tmp = "";
-		tmp += this.getWinScore()+"/"+this.getVisitCount();
-		
+		tmp += this.getWinScore() + "/" + this.getVisitCount();
+
 		return tmp;
 	}
 }
