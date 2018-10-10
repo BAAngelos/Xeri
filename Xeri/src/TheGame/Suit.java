fecce02c4029a66d37eb8f4106a87998b631b610
Angelos <Angelos@DESKTOP-AFUKVS8.fritz.box>
2018-10-10 04:04:24 +0200

    added a getShortcut()method return initial letter of the Suit

--- Xeri/src/TheGame/Suit.java
+++ Xeri/src/TheGame/Suit.java
@@ -2,5 +2,24 @@ package TheGame;
 
 public enum Suit {
 
-	CLUBS, SPADES, DIAMONDS, HEARTS
+	CLUBS, SPADES, DIAMONDS, HEARTS;
+	
+	
+	public String getShortCut() {
+		switch (this) {
+		case CLUBS:
+			return "C";
+		case SPADES:
+			return "S";
+		case DIAMONDS:
+			return "D";
+		case HEARTS: 
+			return "H";
+		default:
+			System.out.println("da ist was falsh");
+			break;
+		}
+		
+		return null;
+	}
 }
